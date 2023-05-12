c7678bd94f4f6f15f84f1bf93ca591c30f309cdc
plbranson <patrick.l.branson@plbranson>
2023-05-12 15:11:09 -0400

    Updated the Project

---
+++ models/File.js
@@ -0,0 +1,36 @@
+/*
+ *  Copyright 2023 Patrick L. Branson
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+const mongoose = require("mongoose");
+
+const File = new mongoose.Schema({
+  path: {
+    type: String,
+    required: true,
+  },
+  originalName: {
+    type: String,
+    required: true,
+  },
+  password: String,
+  downloadCount: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
+
+module.exports = mongoose.model("File", File);
---
+++ server.js
@@ -0,0 +1,85 @@
+/*
+ *  Copyright 2023 Patrick L. Branson
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+const bcrypt = require("bcrypt");
+const multer = require("multer");
+const express = require("express");
+const mongoose = require("mongoose");
+
+// Allows access to .env file(s)
+require("dotenv").config();
+
+const PORT = 8000;
+const app = express();
+
+const upload = multer({ dest: "uploads" });
+
+// The Models File(s)
+const File = require("./models/File");
+
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.DATABASE_URL);
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/upload", upload.single("file"), async (req, res) => {
+  const fileData = {
+    path: req.file.path,
+    originalName: req.file.originalname,
+  };
+
+  if (req.body.password != null && req.body.password !== "") {
+    fileData.password = await bcrypt.hash(req.body.password, 10);
+  }
+
+  const file = await File.create(fileData);
+
+  res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` });
+});
+
+app.route("/file/:id").get(handleDownload);
+app.route("/file/:id").post(handleDownload);
+
+async function handleDownload(req, res) {
+  const file = await File.findById(req.params.id);
+
+  if (file.password != null) {
+    if (req.body.password == null) {
+      res.render("password");
+      return;
+    }
+
+    if (!(await bcrypt.compare(req.body.password, file.password))) {
+      res.render("password", { error: true });
+      return;
+    }
+  }
+
+  file.downloadCount++;
+  await file.save();
+  console.log(file.downloadCount);
+
+  res.download(file.path, file.originalName);
+}
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT: ${PORT}`);
+});
